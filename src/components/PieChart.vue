623c3ea96e9b868bf7d52ec1d8a3a73e9864f4b1
atcha <francois.cornuaud@gmail.com>
2020-06-07 16:57:57 +0200

    Add tooltip and scaling on hover

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,12 +1,15 @@
 <template>
-    <svg :width="pieWidth" :height="pieHeight" viewBox="-1 -1 2 2" style="transform: rotate(-0.25turn)">
+    <svg ref="piechart" :width="pieWidth" :height="pieHeight" viewBox="-1 -1 2 2" style="transform: rotate(-0.25turn)">
         <circle r=".7" cx="0" cy="0" :fill="innerColor"/>
         <circle r=".40" cx="0" cy="0" fill="white"/>
         <path v-for="(path, index) in pathData"
               style="transform: scale(.8)"
+              @mouseover="scaleSlice"
+              @mouseout="scaleSlice"
+              v-tooltip="path.percent + '%'"
               :key="index"
               :d="`M ${path.startX} ${path.startY} A 1 1 0 ${path.largeArcFlag} 1 ${path.endX} ${path.endY} L 0 0 Z`"
-              :fill="path.color" :stroke="path.color" stroke-width=".08" stroke-linejoin="round">
+              :fill="path.color" :stroke="path.color" stroke-width=".07" stroke-linejoin="round">
         </path>
     </svg>
 </template>
@@ -54,18 +57,16 @@
                 this.slices.forEach(slice => {
                     const [startX, startY] = this.getCoordinatesForPercent(cumulativePercent);
                     cumulativePercent += parseFloat(slice.percent);
-                    console.log(cumulativePercent);
                     const [endX, endY] = this.getCoordinatesForPercent(cumulativePercent);
-
                     const largeArcFlag = slice.percent > .5 ? 1 : 0;
-
                     const pathObject = {
                         startX: startX,
                         startY: startY,
                         endX: endX,
                         endY: endY,
                         largeArcFlag: largeArcFlag,
-                        color: slice.color
+                        color: slice.color,
+                        percent: slice.percent * 100
                     };
 
                     paths.push(pathObject);
@@ -78,6 +79,18 @@
                 const y = Math.sin(2 * Math.PI * percent);
 
                 return [x, y];
+            },
+            scaleSlice(event) {
+                this.$refs.piechart.appendChild(event.target);
+                if(event.type === "mouseover") {
+                    window.setTimeout(function() {
+                        event.target.style = 'transform: scale(.9);';
+                    }, 10);
+                } else {
+                    window.setTimeout(function() {
+                        event.target.style = 'transform: scale(.8);';
+                    }, 10);
+                }
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueCircleSlider from 'vue-circle-slider'
+import VTooltip from 'v-tooltip'
 import 'vue-swatches/dist/vue-swatches.css'
+import './assets/css/tooltip.css'
+
 
 Vue.config.productionTip = false
 
 Vue.use(VueCircleSlider)
+Vue.use(VTooltip)
 
 new Vue({
   render: h => h(App),
